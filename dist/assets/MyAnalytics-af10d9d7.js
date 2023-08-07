c17e562f27a72155578c74783d69435ebc7f5947
Suraj <suraj_k@tekditechnologies.com>
2023-08-07 11:36:59 +0530

    build pushed

---
+++ dist/assets/MyAnalytics-af10d9d7.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-8b64dca5.js";function s(){return r.jsx(r.Fragment,{children:r.jsx("div",{children:r.jsx("h2",{children:"Work in Progress"})})})}export{s as default};
---
+++ dist/assets/register.module-7c99ab07.js
@@ -0,0 +1,1 @@
+const o="_formDiv_1124i_1",t="_loginButton_1124i_12",n={formDiv:o,loginButton:t};export{n as s};
